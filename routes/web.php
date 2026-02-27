b4b4816603b080319f30e7619f4bb7d367e4903b
ZukhrufianAbdullah <zukrufianabdullah@gmail.com>
2026-02-27 16:55:44 +0700

    Praktikum 6 – Implementasi Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 // Jobsheet 2
 // use App\Http\Controllers\HomeController;
@@ -18,6 +19,7 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
 // Jobsheet 2
 /* ======================
