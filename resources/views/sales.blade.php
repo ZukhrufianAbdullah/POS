7912394811ccf4e7b9261200acc558a581db4fac
ZukhrufianAbdullah <zukrufianabdullah@gmail.com>
2026-02-27 12:51:26 +0700

    Praktikum 1 - Konfigurasi Database

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -2,5 +2,6 @@
     <body>
         <h1>Halaman Transaksi POS</h1>
         <p>Selamat datang di halaman transaksi Point of Sales kami.</p>
+        <p>Terima kasih telah berbelanja di toko kami!</p>
     </body>
 </html>
